8587b58aaec72ed12029507d542bdb18aa45694b
AmujoDotun <samuelamujo@gmail.com>
2020-04-23 10:52:17 +0100

    getting element by id

---
+++ dom.js
@@ -0,0 +1,18 @@
+// EXAMIN THE DOCUMENT OBJECT
+
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.documentURI);
+// console.log(document.links);
+// console.log(document.forms);
+// console.log(document.title);
+// document.title = "Hello Wolrd";
+// console.log(document.title);
+// console.log(document.all);
+// document.all[8].textContent = 'Hello THere';
+// console.log(document.images);
+
+var headerTitle = document.getElementById('header-title');
+console.log(headerTitle);
+headerTitle.textContent = 'Hello There';
+headerTitle.innerText = "who is there";
