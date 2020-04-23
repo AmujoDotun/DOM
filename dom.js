493f176bfaf1f3cdbc67c041f6ea13e4747b9057
AmujoDotun <samuelamujo@gmail.com>
2020-04-23 11:59:59 +0100

    getting doc objs

--- dom.js
+++ dom.js
@@ -1,16 +1,16 @@
-// EXAMIN THE DOCUMENT OBJECT
+EXAMIN THE DOCUMENT OBJECT
 
-// console.dir(document);
-// console.log(document.domain);
-// console.log(document.documentURI);
-// console.log(document.links);
-// console.log(document.forms);
-// console.log(document.title);
-// document.title = "Hello Wolrd";
-// console.log(document.title);
-// console.log(document.all);
-// document.all[8].textContent = 'Hello THere';
-// console.log(document.images);
+console.dir(document);
+console.log(document.domain);
+console.log(document.documentURI);
+console.log(document.links);
+console.log(document.forms);
+console.log(document.title);
+document.title = "Hello Wolrd";
+console.log(document.title);
+console.log(document.all);
+document.all[8].textContent = 'Hello THere';
+console.log(document.images);
 
 // var headerTitle = document.getElementById('header-title');
 // console.log(headerTitle);
