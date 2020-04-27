711c3901287ecd89af2b4487854402c7474a37e7
AmujoDotun <samuelamujo@gmail.com>
2020-04-27 20:33:11 +0100

    make used of the run and move event also make the backeground color change using rgb formate when the mouse is moved around the box created

--- dom.js
+++ dom.js
@@ -153,8 +153,13 @@ var box = document.getElementById('box');
 
 // box.addEventListener('mouseout', runEvent);
 // box.addEventListener('mouseover', runEvent);
-// box.addEventListener('mousemove', runEvent);
+box.addEventListener('mousemove', runEvent);
+
+
 function runEvent(e)
 {
     console.log('EVENT TYPE: ' +e.type);
+    // output.innerHTML = '<h3>MouseX:' +e.offsetX +'</h3><h3> MouseY :'+e.offsetY +'</h3>';
+    document.body.style.backgroundColor = "rgb("+e.offsetX+", "+e.offsetY+", 40)";
 }
+
